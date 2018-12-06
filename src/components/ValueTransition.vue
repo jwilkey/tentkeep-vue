f9bb677c70bdf401ead08cc60660b06a2c7be89c
Justin Wilkey <jwilkey@Justins-MacBook-Pro.local>
2018-12-06 00:36:05 -0500

    Add ValueTransition

---
+++ src/components/ValueTransition.vue
@@ -0,0 +1,39 @@
+<template>
+  <transition :name="transition">
+    <div class="value-transition" v-if="display">{{ display }}</div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: 'ValueTransition',
+  data () {
+    return {
+      display: undefined
+    }
+  },
+  props: ['value', 'name'],
+  computed: {
+    transition () {
+      return this.name || 'fade'
+    }
+  },
+  watch: {
+    value () {
+      this.display = undefined
+      this.$nextTick(() => {
+        this.display = this.value
+      })
+    }
+  },
+  mounted () {
+    this.display = this.value
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.value-transition {
+  display: inline-block;
+}
+</style>
